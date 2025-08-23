61dee8609bd314cc3831c498d7096a0af890643d
Edu Pagotto <edupagotto@gmail.com>
2025-08-23 14:43:01 -0300

    format .s files

--- samples/driver/interrupts.s
+++ samples/driver/interrupts.s
@@ -1,25 +1,25 @@
-.section .text # Code area
-.globl _start # entry point
+    .section .text                               # Code area
+    .globl   _start                              # entry point
 
-#  Default values
-.equ CSR_MSTATUS, 0x00000008
-.equ CSR_MISA, 0x40000100
-.equ CSR_MIE, 0x00000888
+# Default values
+    .equ     CSR_MSTATUS, 0x00000008
+    .equ     CSR_MISA, 0x40000100
+    .equ     CSR_MIE, 0x00000888
 
 _start:
-    # initialize sp
-    la sp,_stack_top
-    # load default vals into CSR
-    li t0, CSR_MSTATUS
-    csrrw zero, mstatus, t0
-    li t0, CSR_MISA
-    csrrw zero, misa, t0
-    li t0, CSR_MIE
-    csrrw zero, mie, t0
-    la t0, exceptions_vector
-    csrrw zero, mtvec, t0
+# initialize sp
+    la       sp,_stack_top
+# load default vals into CSR
+    li       t0, CSR_MSTATUS
+    csrrw    zero, mstatus, t0
+    li       t0, CSR_MISA
+    csrrw    zero, misa, t0
+    li       t0, CSR_MIE
+    csrrw    zero, mie, t0
+    la       t0, exceptions_vector
+    csrrw    zero, mtvec, t0
     nop
-    addi t0, zero, 0
+    addi     t0, zero, 0
     ecall
     nop
     nop
@@ -28,38 +28,38 @@ _start:
     wfi
 
 exceptions_vector:
-    j InstructionAddressMisaligned
-    j InstructionAccessFault
-    j IllegalInstruction
-    j Breakpoint
-    j LoadAddressMisaligned
-    j LoadAccessFault
-    j StoreAMOAddressMisaligned
-    j StoreAMOAccessFault
-    j EnvironmentCallFromUMode
-    j EnvironmentCallFromSMode
-    j Reserved3
-    j EnvironmentCallFromMMode
-    j InstructionPageFault
-    j LoadPageFault
-    j Reserved4
-    j StoreAMOPageFault
+    j        InstructionAddressMisaligned
+    j        InstructionAccessFault
+    j        IllegalInstruction
+    j        Breakpoint
+    j        LoadAddressMisaligned
+    j        LoadAccessFault
+    j        StoreAMOAddressMisaligned
+    j        StoreAMOAccessFault
+    j        EnvironmentCallFromUMode
+    j        EnvironmentCallFromSMode
+    j        Reserved3
+    j        EnvironmentCallFromMMode
+    j        InstructionPageFault
+    j        LoadPageFault
+    j        Reserved4
+    j        StoreAMOPageFault
     ;
-    j UserSoftwareInterrupt
-    j SupervisorSoftwareInterrupt
-    j Reserved0
-    j MachineSoftwareInterrupt
-    j UserTimerInterrupt
-    j SupervisorTimerInterrupt
-    j Reserved1
-    j MachineTimerInterrupt
-    j UserExternalInterrupt
-    j SupervisorExternalInterrupt
-    j Reserved2
-    j MachineExternalInterrupt
-    #
-    # Exceptions
-    #
+    j        UserSoftwareInterrupt
+    j        SupervisorSoftwareInterrupt
+    j        Reserved0
+    j        MachineSoftwareInterrupt
+    j        UserTimerInterrupt
+    j        SupervisorTimerInterrupt
+    j        Reserved1
+    j        MachineTimerInterrupt
+    j        UserExternalInterrupt
+    j        SupervisorExternalInterrupt
+    j        Reserved2
+    j        MachineExternalInterrupt
+#
+# Exceptions
+#
 InstructionAddressMisaligned:
     mret
 InstructionAccessFault:
@@ -123,13 +123,16 @@ MachineExternalInterrupt:
     mret
 
 
-// Global Vals
-.section .rodata // Constants
-msg_erroc: .string "erro fatal\n"
+    //       Global Vals
+    .section .rodata // Constants
+msg_erroc:
+    .string  "erro fatal\n"
 
-.section .data   // rw inicializadas com valor
-data01: .4byte 0x00112233
+    .section .data // rw inicializadas com valor
+data01:
+    .4byte   0x00112233
 
-.section .bss
-value1: .word 0x0
-.end
+    .section .bss
+value1:
+    .word    0x0
+    .end
